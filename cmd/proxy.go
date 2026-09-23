019e32153f97d3d53da84dbb36dc4e66e9a29c4e
Pablo Muñoz <pablomunoz@pablomunoz.dev>
2026-09-23 21:21:45 +0200

    clean redundancy in alias cmd

--- cmd/proxy.go
+++ cmd/proxy.go
@@ -17,28 +17,17 @@ var proxyCmd = &cobra.Command{
 	Args:      cobra.ExactArgs(1),
 	ValidArgs: []string{"start", "stop"},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		isRunning, err := client.IsDaemonRunning()
-		if err != nil {
-			return err
-		}
-
 		switch args[0] {
 		case "start":
-			if isRunning {
-				client.Info("Already running\n")
-				return nil
-			}
 			if err := client.EnsureProxy(); err != nil {
 				return err
 			}
+			client.Info("Proxy started\n")
 		case "stop":
-			if !isRunning {
-				client.Info("Already stopped\n")
-				return nil
-			}
 			if err := client.StopProxy(); err != nil {
 				return err
 			}
+			client.Info("Proxy stopped\n")
 		default:
 			return fmt.Errorf("arg is not valid")
 		}
